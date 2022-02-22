f3e26342a47db4d29b33f9f6f83f753821ab08c7
DincProductions <100015093+DincProductions@users.noreply.github.com>
2022-02-22 11:31:20 +0300

    Add files via upload

---
+++ JS Show Dependent.js
@@ -0,0 +1,16 @@
+<script>
+function Degerata() {
+let x,y;
+x=document.getElementById("Xvalue").value;
+y=x*document.getElementById("Yvalue").value;
+
+
+ document.getElementById("demo").innerHTML = "The value of y is "  + y + "." ;
+}
+function Deger() {
+let x,y;
+y=document.getElementById("Yvalue").value;
+x=y*document.getElementById("Xvalue").value;
+ document.getElementById("demo").innerHTML = "Rate Of Change is "  + y + "." ;
+}
+</script>
